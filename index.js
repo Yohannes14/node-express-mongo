bea3fcd911f6cd44c4346f95dcfb000eb429930c
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-06-06 09:15:20 +0300

    crud operation

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/error");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+connectDB();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/product", require("./routes/productRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on ${port}`));
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,18 +4,17 @@ const {
   postProduct,
   updateProduct,
   delteProduct,
+  getProduct,
 } = require("../controller/productController");
 
 const router = express.Router();
 
-router.route("/").get(getProducts).post(postProduct);
-router.route("/:id").delete(delteProduct).put(updateProduct);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 
-// router.get("/", getProduct);
+router.post("/", postProduct);
 
-// router.post("/", postProduct);
-
-// router.put("/:id", updateProduct);
-// router.delete("/:id", delteProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", delteProduct);
 
 module.exports = router;
